090bb9083840d8d0b02047de3a49215330ddf881
jihooncho <george@gamevilcom2us.com>
2021-02-27 23:54:47 +0900

    gatsby antd

--- _posts/2019-03-11-test-stackoverflow-link.markdown
+++
@@ -1,9 +0,0 @@
----
-layout: post
-title:  "스택오버플로우 링크"
-date:   2019-03-11 11:13:27 +0900
-categories: jekyll update
----
-
-[StackOverflow](https://stackoverflow.com/users/6375098/jo8937)
-
---
+++ blog/gatsby-config.js
@@ -0,0 +1,23 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+const config = require('./config');
+const plugins = require('./gatsby-config.plugins');
+
+require("dotenv").config({
+  path: `.env`,
+})
+
+module.exports = {
+  pathPrefix: config.pathPrefix,
+  siteMetadata: {
+    // Data used by some gatsby plugins
+    siteUrl: config.siteUrl,
+    title: config.siteTitle,
+    description: config.siteDescription,
+  },
+  plugins,
+};
